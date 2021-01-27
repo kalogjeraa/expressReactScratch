9b07de3e62c746e08ca57b373f6c553057d3ee69
guinevere <kalogjeraa@gmail.com>
2021-01-27 06:05:52 -0500

    add view engine to app.js

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 app = express();
 
 require('dotenv').config();
+app.set('view engine', 'ejs');
+app.use('/', require('./routes/hello'));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}`);
-})
+});
